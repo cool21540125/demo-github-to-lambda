c24fc3fc2bdac49ea5ac1b24b332f3fa93e74cb7
Tony Chou <cool21540125@gmail.com>
2022-08-24 15:31:16 +0800

    good

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,8 @@
-from dateutil.relativedelta import datetime
+from datetime import datetime
+from dateutil.relativedelta import *
 
 def lambda_handler(event, context):
     now = datetime.now()
+    now += relativedelta(months=1)
     print(now)
     print("DONE")
